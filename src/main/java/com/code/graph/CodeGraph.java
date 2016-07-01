f5951ca5f4880a898e96ff1e5fee62d91ab5c588
Julio <jmcorral2@miners.utep.edu>
2016-06-30 18:05:16 -0600

    edited codegraph.java
    Added some visit functions
    -to display conditions in: if-statemnts
    -to display body of while and for loops

--- src/main/java/com/code/graph/CodeGraph.java
+++ src/main/java/com/code/graph/CodeGraph.java
@@ -7,7 +7,12 @@ import java.util.*;
 
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.WhileStmt;
+import com.github.javaparser.ast.visitor.GenericVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.sun.glass.ui.EventLoop;
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -31,6 +36,7 @@ public class CodeGraph
         try {
             // parse the file
             cu = JavaParser.parse(in);
+            
         } finally {
             in.close();
         }
@@ -40,37 +46,53 @@ public class CodeGraph
         // System.out.println(cu.toString()); */
 
         System.out.println("Visiting Methods in FractionalToBinary.java");
-        new MethodVisitor().visit(cu, null);
+        new MethodVisitorTest().visit(cu, null);
 
     }
 
+  
+   
+    
     /**
      * Simple visitor implementation for visiting MethodDeclaration nodes.
      */
-    private static class MethodVisitor extends VoidVisitorAdapter {
+    private static class MethodVisitorTest extends VoidVisitorAdapter {
 
         @Override
         public void visit(MethodDeclaration n, Object arg) {
             // here you can access the attributes of the method.
             // this method will be called for all methods in this
             // CompilationUnit, including inner class methods
-            System.out.println(n.getName());//Gets method names.
-            System.out.println(n.getBody());//Gets contents of method.
+//            System.out.println(n.getName());//Gets method names.
+//            System.out.println(n.getBody().getChildrenNodes());//Gets contents of method.
 
             List<Statement> statements;
+            List<IfStmt> current_ifs;
+            List<ForStmt> current_fors;
+            
+           
 
             //Test to see if we can get contents of method and set them in a block.
-            System.out.print("Statements are: ");
+            System.out.println("----------------Statements are: -------------------");
+            
             BlockStmt block = new BlockStmt();
+            IfStmt conditional_IF = new IfStmt();
+            ForStmt iterative_FOR = new ForStmt();
+            
             block = n.getBody();
-
+            
+            
             statements = block.getStmts();
-            int x =0;
+            
+            int x =1;
             for(Statement statement: statements){
-                System.out.println(x + " ");
+                System.out.println("("+ x + ") ");
                 System.out.println(statement);
+                
                 x++;
             }
+            System.out.println();
+             System.out.println("------------------------------------------------");
             //Ok so, in Java blocks are executed as a single statement. That's why if you run this, you can see the statement separated by commas and a block is seen as a statement.
             //What's a block in java? https://docs.oracle.com/javase/tutorial/java/nutsandbolts/expressions.html
 
@@ -78,6 +100,40 @@ public class CodeGraph
 
 
         }
+        
+        @Override
+        public void visit(IfStmt n, Object org){
+            
+            
+            System.out.println("CURRENT if CONDITIONS found--> "+n.getCondition());
+//            System.out.println("ALL IF data "+n.getData());
+            super.visit(n,org);
+            
+        }
+        
+        @Override
+        public void visit(ForStmt n, Object org){
+            
+            
+            System.out.println("CONDITION in the FOR--> "+n.getCompare());
+            System.out.println("BODY inside each FORLOOP"+n.getBody());
+            super.visit(n,org);
+            
+        }
+        
+         @Override
+        public void visit(WhileStmt n, Object org){
+            
+            
+            System.out.println("CONDITION in the WHILE"+n.getCondition());
+            System.out.println("BODY inside each WHILELOOP "+n.getBody());
+            super.visit(n,org);
+            
+        }
+        
     }
+    
+    
+   
 
 }
